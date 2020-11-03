aef941325d395ca49059007ad928bdcdb1f8b07e
MomohNobert <okhai0112@gmail.com>
2020-11-03 23:57:39 +0100

    hello mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema.js');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.once('open', () => {
+    console.log("Connected to Database.")
+})
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
 app.listen(5000, () => {
-    console.log("Listening for requests on port 5000")
+    console.log("Listening for requests on port 5000");
 })
